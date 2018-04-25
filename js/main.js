6479b8b4037b46a68ca1e1c24a4d382aad1c793a
Confidence Okoghenun <confidenceboi@gmail.com>
2018-04-25 15:19:44 +0100

    Random Color Generator
    This commit includes changes that has made
    it possible for random colors to be generated
    each time the page is reloaded.
    
    *The two functions 'generateRandomColors' and
     'randomColor' are used to produce random colors
     in string form and push them into an array.
    *This change has made it possible for the game
     colors not to be hard coded.
    *Next up would be ui changes and performance
     improvement.
    
    [Closes-Bug: #003]

--- js/main.js
+++ js/main.js
@@ -1,17 +1,11 @@
-var colors = [
-  "rgb(255, 9, 0)",
-  "rgb(25, 90, 0)",
-  "rgb(5, 109, 200)",
-  "rgb(200, 159, 250)",
-  "rgb(255, 9, 255)",
-  "rgb(255, 249, 0)"
-];
+var colors = generateRandomColors(6);
 
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedColor;
 var squares = document.querySelectorAll(".square");
-var messageDisplay = document.querySelector("#message")
+var messageDisplay = document.querySelector("#message");
+var h1 = document.querySelector("h1");
 for(var i = 0; i < squares.length; i++) {
   //add initail color to squares
     squares[i].style.background = colors[i];
@@ -21,6 +15,7 @@ for(var i = 0; i < squares.length; i++) {
     if (pickedColor === clickedColor) {
       messageDisplay.textContent = "Correct";
       changeColor(pickedColor);
+      h1.style.background = pickedColor;
      } else {
       this.style.background = "#232323";
       messageDisplay.textContent = "Try Again"
@@ -37,4 +32,21 @@ function changeColor(color) {
 function pickColor() {
   var random = Math.floor(Math.random() * colors.length);
   return colors[random];
+}
+
+function generateRandomColors(num) {
+  var arr = [];
+
+  for(var i = 0; i < num; i++) {
+    arr.push(randomColor());
+  }
+  return arr;
+}
+
+function randomColor() {
+  var r = Math.floor(Math.random() * 256);
+  var g = Math.floor(Math.random() * 256);
+  var b = Math.floor(Math.random() * 256);
+  "rgb(r, g, b)"
+  return "rgb(" + r + ", " + g + ", " + b + ")";
 }
