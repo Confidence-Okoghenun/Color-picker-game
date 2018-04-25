5c68304e949cdcaf7d4c1d199c139c3876c03f39
Confidence Okoghenun <confidenceboi@gmail.com>
2018-04-25 17:10:58 +0100

    First Stable Version
    This commit is the first stable version with
    good ui and it functions optimally.
    
    *Ui and performance changes were made.
    *This has made the game playable and stable.
    *No idea for future improvements.
    
    [Closes-Bug: #004]

--- js/main.js
+++ js/main.js
@@ -1,11 +1,57 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedColor;
 var squares = document.querySelectorAll(".square");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
+var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easy");
+var hardButton = document.querySelector("#hard");
+
+resetButton.addEventListener("click", function() {
+  resetButton.textContent = "New Color";
+  h1.style.background = "#steelblue";
+  colors = generateRandomColors(numSquares);
+  messageDisplay.textContent = "";
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+  };
+});
+
+easyButton.addEventListener("click", function() {
+  for(var i = 3; i < squares.length; i++) {
+    squares[i].style.display = "none";
+  };
+  easyButton.classList.add("selected");
+  hardButton.classList.remove("selected");
+  numSquares = 3;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+  };
+});
+
+hardButton.addEventListener("click", function() {
+  for (var i = 3; i < squares.length; i++) {
+    squares[i].style.display = "block";
+  };
+  hardButton.classList.add("selected");
+  easyButton.classList.remove("selected");
+  numSquares = 6;
+  colors = generateRandomColors(numSquares);
+  pickedColor = pickColor();
+  colorDisplay.textContent = pickedColor;
+  for (var i = 0; i < squares.length; i++) {
+    squares[i].style.background = colors[i];
+  };
+});
+
 for(var i = 0; i < squares.length; i++) {
   //add initail color to squares
     squares[i].style.background = colors[i];
@@ -16,6 +62,7 @@ for(var i = 0; i < squares.length; i++) {
       messageDisplay.textContent = "Correct";
       changeColor(pickedColor);
       h1.style.background = pickedColor;
+      resetButton.textContent = "Play Again?"
      } else {
       this.style.background = "#232323";
       messageDisplay.textContent = "Try Again"
