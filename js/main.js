2af12a689149a481e2801d4776b125bb37349744
Confidence Okoghenun <confidenceboi@gmail.com>
2018-04-25 14:42:33 +0100

    Click Event Handler
    This commit include changes to handle the
    click event of each color tile.
    
    *A function was created to handle click event
     for each color tile and determine if the clicked
     color was the correct one.
    *This has made it possible for the correct color
     to be determined.
    *Next feature would be to generate colors randomly.
    
    [Closes-Bug: #002]

--- js/main.js
+++ js/main.js
@@ -1,27 +1,40 @@
 var colors = [
-  "rgb(255,9,0)",
-  "rgb(25,90,0)",
-  "rgb(5,109,200)",
-  "rgb(200,159,250)",
-  "rgb(255,9,255)",
-  "rgb(255,249,0)"
+  "rgb(255, 9, 0)",
+  "rgb(25, 90, 0)",
+  "rgb(5, 109, 200)",
+  "rgb(200, 159, 250)",
+  "rgb(255, 9, 255)",
+  "rgb(255, 249, 0)"
 ];
 
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.querySelector("#colorDisplay");
 colorDisplay.textContent = pickedColor;
 var squares = document.querySelectorAll(".square");
+var messageDisplay = document.querySelector("#message")
 for(var i = 0; i < squares.length; i++) {
   //add initail color to squares
     squares[i].style.background = colors[i];
-
   //add event listener to squares
   squares[i].addEventListener("click", function() {
     var clickedColor = this.style.background;
-    if(clickedColor === pickedColor) {
-
-    } else {
-      this.style.background = document.body.style.background;
+    if (pickedColor === clickedColor) {
+      messageDisplay.textContent = "Correct";
+      changeColor(pickedColor);
+     } else {
+      this.style.background = "#232323";
+      messageDisplay.textContent = "Try Again"
     }
   });
+}
+
+function changeColor(color) {
+  for(var i = 0; i < squares.length; i++) {
+    squares[i].style.background = color;
+  }
+};
+
+function pickColor() {
+  var random = Math.floor(Math.random() * colors.length);
+  return colors[random];
 }
